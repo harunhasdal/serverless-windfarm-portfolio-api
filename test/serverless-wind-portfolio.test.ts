6c7483b91911ea9440e7d04c498ba44ce1518bca
Harun Hasdal <harunhasdal@gmail.com>
2023-02-13 10:56:10 +0000

    Initial commit

---
+++ test/serverless-wind-portfolio.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessWindPortfolio from '../lib/serverless-wind-portfolio-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-wind-portfolio-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessWindPortfolio.ServerlessWindPortfolioStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
